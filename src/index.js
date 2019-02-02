a4e45072b749d2754b6618c54efa8d7c9fab18ea
Nat Grant <email.natgrant@gmail.com>
2019-02-02 13:13:20 +1300

    adding babel presets

--- src/index.js
+++ src/index.js
@@ -3,6 +3,4 @@ import ReactDOM from "react-dom";
 
 import App from "./components/App";
 
-document.addEventListener("DOMContentLoaded", () => {
-  ReactDOM.render(<App />, document.getElementById("app"));
-});
+ReactDOM.render(<App />, document.getElementById("app"));
