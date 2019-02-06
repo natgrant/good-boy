2b2aecd9174b7ece2d8c9660e34711eec31f6d15
Nat Grant <email.natgrant@gmail.com>
2019-02-07 11:20:58 +1300

    server-side setup

--- server/server.js
+++ server/server.js
@@ -1,9 +1,16 @@
 const path = require("path");
 const express = require("express");
 
+// Set up server-side routes e.g. :
+const dogs = require("./routes/dogs");
+
 const server = express();
 
 server.use(express.json());
 server.use(express.static(path.join(__dirname, "./public")));
+
+// Set up paths for server.use()
+
+// Server.get index.html?
 
 module.exports = server;
