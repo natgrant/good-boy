3d29cc20b7403f1a07638ea5659ff61ef5ca7fcc
Nat Grant <email.natgrant@gmail.com>
2019-02-10 08:58:30 +1300

    server missing api route

--- server/server.js
+++ server/server.js
@@ -1,16 +1,15 @@
 const path = require("path");
 const express = require("express");
-
-// Set up server-side routes e.g. :
-const dogs = require("./routes/dogs");
-
 const server = express();
+const bodyParser = require("body-parser");
+// Set up server-side routes e.g. :
+const routes = require("./routes/main");
 
+server.use(bodyParser.json());
 server.use(express.json());
-server.use(express.static(path.join(__dirname, "./public")));
+server.use(express.static(path.join(__dirname, "../public")));
 
 // Set up paths for server.use()
-
-// Server.get index.html?
+server.use("/api", routes);
 
 module.exports = server;
