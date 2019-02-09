f1bacc69d4ed98e65a6f9a8f9443f0c54d11669e
Nat Grant <email.natgrant@gmail.com>
2019-02-10 08:56:52 +1300

    setting up apis

---
+++ src/api/users.js
@@ -0,0 +1,10 @@
+import request from "superagent";
+
+export function getUsers() {
+  return request.get("/api");
+  then(res => {
+    return res.body;
+  });
+}
+
+// client side needs to speak to DB
