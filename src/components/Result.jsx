586eddbc524878d2bdc24e0f2bebcc1804126d46
Nat Grant <email.natgrant@gmail.com>
2019-02-12 22:43:18 +1300

    showing b a s i c result

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,13 +1,23 @@
 import React from "react";
 
-const Result = props => {
-  return (
-    <React.Fragment>
-      <div className="display-dog">
-        <h1>Your result here!</h1>
-      </div>
-    </React.Fragment>
-  );
-};
+class Result extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    if (this.props.isShowing) {
+      return (
+        <React.Fragment>
+          <div className="display-dog">
+            <h1>Your dog!</h1>
+            <p>{this.props.dog}</p>
+          </div>
+        </React.Fragment>
+      );
+    } else {
+      return null;
+    }
+  }
+}
 
 export default Result;
