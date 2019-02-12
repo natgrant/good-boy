22f63238bcede2816450386be1f5facdf685b675
Nat Grant <email.natgrant@gmail.com>
2019-02-12 23:30:44 +1300

    remove one polygon to change style

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -7,15 +7,6 @@ const Parallex = () => {
         <svg preserveAspectRatio="none" viewBox="0 0 100 100">
           <polygon points="100,100 100,0 0,100" opacity="1" />
         </svg>
-        <svg
-          id="polygon-1"
-          x="0px"
-          y="0px"
-          viewBox="0 0 1440 270"
-          enableBackground="new 0 0 1440 270"
-        >
-          <polygon points="1440,0 0,230 0,270 1440,40 " />
-        </svg>
       </div>
     </div>
   );
